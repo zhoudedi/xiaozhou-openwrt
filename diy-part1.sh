c815adce5dddec93efaa82c161dde5b0639eb486
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-02-13 13:57:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,11 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-git clone https://github.com/OpenWrt-Actions/openwrt-package-lienol package/openwrt-package-lienol
+#git clone https://github.com/OpenWrt-Actions/openwrt-package-lienol package/openwrt-package-lienol
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git >>feeds.conf.default
