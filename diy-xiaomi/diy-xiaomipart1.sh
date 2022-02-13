e958b2acc05969509eab8968e5d861c86664c171
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-02-13 15:14:04 +0800

    Update diy-xiaomipart1.sh

--- diy-xiaomi/diy-xiaomipart1.sh
+++ diy-xiaomi/diy-xiaomipart1.sh
@@ -14,6 +14,6 @@ git clone https://github.com/OpenWrt-Actions/openwrt-package-lienol package/open
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
